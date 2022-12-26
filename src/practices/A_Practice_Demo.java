4846fcc9e21bfb083cdb189940da249c5db2278a
sakti <dhalsakti@gmail.com>
2022-12-26 18:06:41 +0530

    nh

--- src/practices/A_Practice_Demo.java
+++ src/practices/A_Practice_Demo.java
@@ -4,16 +4,26 @@ public class A_Practice_Demo {
 	public static void main(String[] args) {
 		
 	
-		String s = "bikerboyk";
+		String str = "ggittttaaa";
+		System.out.println("Original string is :"+str);
 		
-		for(int i=0;i<s.length();i++)
+		StringBuffer s = new StringBuffer();
+		
+		for(int i=0;i<str.length();i++)
 		{
-			for(int j=i+1;j<s.length();j++)
+			char ch = str.charAt(i);
+			int index = str.indexOf(ch, i+1);
+			if(index==-1)
 			{
-				if(s.charAt(i)==s.charAt(j))
-					System.out.println(s.charAt(i));
+				s.append(ch);
 			}
 		}
+		System.out.println("String without duplicate is : "+s);
+		
+		
+		
+		
+		
    }
 	
 }
